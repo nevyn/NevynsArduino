01ef5f5d6074b1eff88cb8c25feb16e956a2b8b5
Nevyn Bengtsson <hello@nevyn.dev>
2024-05-19 01:15:30 +0200

    mediakeeb: use the led

--- mediakeeb/mediakeeb.ino
+++ mediakeeb/mediakeeb.ino
@@ -2,6 +2,7 @@
 #include <USBHIDKeyboard.h>
 #include <USBHIDConsumerControl.h>
 #include <M5Unified.h>
+#include <FastLED.h> 
 
 #if !defined(ARDUINO_USB_MODE)
 #error Needs an M5AtomS3, which has USB support
@@ -12,6 +13,10 @@ USBHIDConsumerControl cons;
 static const unsigned long debounce = 50;
 // if I wanted to do a bluetooth keyboard: https://gist.github.com/manuelbl/66f059effc8a7be148adb1f104666467 or https://hackaday.com/2020/02/13/emulating-a-bluetooth-keyboard-with-the-esp32/
 // Based on https://github.com/espressif/arduino-esp32/blob/master/libraries/USB/examples/Keyboard/KeyboardMessage/KeyboardMessage.ino
+
+CRGB leds[1];
+void LEDSetup() { FastLED.addLeds<WS2811, 35, GRB>(leds, 1);FastLED.setBrightness(20); }
+void LEDIndicate(CRGB color) { leds[0] = color; FastLED.show(); }
 
 class MediaKey {
 public:
@@ -37,9 +42,11 @@ public:
     lastReading = reading;
   }
   void on() {
+    LEDIndicate(CRGB(0,50,0));
     cons.press(equiv);
   }
   void off() {
+    LEDIndicate(CRGB(50,00,0));
     cons.release();
   }
   int pin;
@@ -56,6 +63,8 @@ MediaKey *keys[] = {&prev, &next, &play};
 
 void setup() {
   M5.begin();
+  LEDSetup();
+  LEDIndicate(CRGB(50,0,0));
   for(int i = 0; i < 3;  i++)
     keys[i]->setup();
   
@@ -70,7 +79,6 @@ void loop() {
   for(int i = 0; i < 3;  i++)
     keys[i]->update();
   if(M5.BtnA.isPressed()) {
-    cons.press(CONSUMER_CONTROL_SLEEP);
-    cons.release();
+    // ...
   }
 }
